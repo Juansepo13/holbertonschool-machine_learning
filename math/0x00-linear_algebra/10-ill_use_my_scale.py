e26d4fe344598b4dd53bbfe61dc8c158895becf0
Juansepo13 <juancho_s1314@hotmail.com>
2022-07-28 15:24:15 -0500

    add file 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module to calculate the shape of a matrix using numpy
+"""
+
+
+def np_shape(matrix):
+    """
+    Needs a matrix as input
+    Returns the shape as a list of integers
+    """
+    return matrix.shape
