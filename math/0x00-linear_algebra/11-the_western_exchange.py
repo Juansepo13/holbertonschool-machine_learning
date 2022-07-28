a7de2d5a1514b227dd9c6b8fecbbfcbb29f3b162
Juansepo13 <juancho_s1314@hotmail.com>
2022-07-28 15:26:14 -0500

    add all files

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that trasposes a matrix using numpy
+"""
+
+
+def np_transpose(matrix):
+    """
+    Needs a matrix as input
+    Returns a trasposed matrix
+    """
+    return matrix.transpose()
