6f5f86dbb1969b44941aa816f193a3615c5b0fb6
Juansepo13 <juancho_s1314@hotmail.com>
2022-08-22 11:22:47 -0500

    add file 1

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,51 +1,36 @@
 #!/usr/bin/env python3
-"""Module containing the class Neuron which defines a single neuron performing
-binary classification"""
-
+"""
+Module for class Neuron
+"""
 import numpy as np
 
 
-class Neuron():
-    """Class which defines a single neuron performing binary classification
+class Neuron:
     """
-    def __init__(self, nx):
-        """Initizilation function for Neuron
+    Defines a single neuron performing binary classification
+    """
 
-        Args:
-            nx (int): The number of input features to the neuron
-        """
-        if not isinstance(nx, int):
+    def __init__(self, nx):
+        """ Class constructor """
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        elif nx < 1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
-        else:
-            self.__W = np.random.randn(1, nx)
-            self.__b = 0
-            self.__A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
-        """Getter for private instance atribute W.
-
-        Returns:
-            [float]: The weights vector for the neuron.
-        """
+        """ Getter function for W """
         return self.__W
 
     @property
     def b(self):
-        """Getter for private instance atribute b
-
-        Returns:
-            [float]: The bias for the neuron.
-        """
+        """ Getter function for b """
         return self.__b
 
     @property
     def A(self):
-        """Getter for private instance atribute
-
-        Returns:
-            [numpy.ndarray]: The activated output of the neuron.
-        """
+        """ Getter function for A """
         return self.__A
