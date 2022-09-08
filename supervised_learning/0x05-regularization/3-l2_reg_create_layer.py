461e9612f5474baa1108d4765b0090a369663229
Juansepo13 <juancho_s1314@hotmail.com>
2022-09-08 11:50:07 -0500

    asas

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-""" Layer with tensorflow including L2 regularization """
+'''Create a Layer with L2 Regularization tensorflow module'''
 import tensorflow as tf
 
+
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """ Layer with tensorflow including L2 regularization """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    '''creates a tensorflow layer that includes L2 regularization
+    Args:
+        prev is a tensor containing the output of the previous layer
+        n is the number of nodes the new layer should contain
+        activation is the activation function that should be used on the layer
+        lambtha is the L2 regularization parameter
+    Returns: the output of the new layer
+    '''
+    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     regularizer = tf.contrib.layers.l2_regularizer(lambtha)
-    layer = tf.layers.Dense(n, activation, name='layer',
-                            kernel_initializer=init,
+    layer = tf.layers.Dense(n,
+                            activation=activation,
+                            kernel_initializer=w,
                             kernel_regularizer=regularizer)
-    out = layer(prev)
-    return out
+    return layer(prev)
