a10a19058107296acdc0a1daf649ba718f68aebb
Juansepo13 <juancho_s1314@hotmail.com>
2022-09-08 15:22:59 -0500

    add file 5

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -1,39 +1,49 @@
 #!/usr/bin/env python3
 """
-Dropout gradient descent
+Gradient Descent with Dropout
 """
-
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Y is a one-hot numpy.ndarray of shape (classes, m)
-    that contains the correct labels for the data
+    Dropout regularization using gradient descent
+    Args:
+        Y: (classes, m) that contains the correct labels for the data
         classes is the number of classes
         m is the number of data points
-    weights is a dictionary of the weights and biases of
-    the neural network
-    cache is a dictionary of the outputs and dropout masks
-    of each layer of the neural network
-    alpha is the learning rate
-    keep_prob is the probability that a node will be kept
-    L is the number of layers of the network
-    All layers use thetanh activation function except the last,
-    which uses the softmax activation function
-    The weights of the network should be updated in place
+        weights: is a dictionary of the weights and biases of the DNN
+        cache: is a dictionary of the outputs and dropout
+            masks of each layer of the neural network
+        alpha: is the learning rate
+        keep_prob is the probability that a node will be kept
+        L is the number of layers of the network
     """
     m = Y.shape[1]
-    weights_cop = weights.copy()
+    cweights = weights.copy()
     for i in range(L, 0, -1):
-        A = cache['A' + str(i)]
         if i == L:
-            dz = A - Y
+            # Current layer error, derivate cost respect to Z
+            curr_layer_err = cache['A'+str(i)] - Y
         else:
-            dz = np.matmul(weights_cop['W' + str(i + 1)].T, dz) * (1 - (A * A))
-            dz = dz * cache['D' + str(i)]
-            dz = dz / keep_prob
-        dw = 1 / m * np.matmul(dz, cache['A' + str(i - 1)].T)
-        db = 1 / m * np.sum(dz, axis=1, keepdims=True)
-        weights["W" + str(i)] = weights_cop['W' + str(i)] - alpha * dw
-        weights["b" + str(i)] = weights_cop['b' + str(i)] - alpha * db
+            factor = np.dot(cweights['W'+str(i+1)].T, prev_layer_err)
+            # Current layer error
+            curr_layer_err = factor * derv_tanh(cache['A' + str(i)])
+            # aplying mask to current layer error
+            curr_layer_err *= cache['D'+str(i)] / keep_prob
+        # derivate cost respect to weight
+        derv_cost_w = np.dot(curr_layer_err, cache['A' + str(i-1)].T) / m
+        # derivate cost respect to bias
+        derv_cost_b = np.sum(curr_layer_err, axis=1, keepdims=True) / m
+        # Update weights and bias
+        weights['W'+str(i)] = cweights['W'+str(i)] - alpha * derv_cost_w
+        weights['b'+str(i)] = cweights['b'+str(i)] - alpha * derv_cost_b
+        # Update layer error
+        prev_layer_err = curr_layer_err
+
+
+def derv_tanh(A):
+    """
+    Derivate of tanH
+    """
+    return 1 - (A**2)
