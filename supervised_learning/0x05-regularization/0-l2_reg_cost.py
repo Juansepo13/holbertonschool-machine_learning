d38733d4fc8e3d502e0774104e1689d894199a56
Juansepo13 <juancho_s1314@hotmail.com>
2022-09-08 10:36:25 -0500

    file for task0

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """ Cost with L2 regularization"""
-    w_norm = 0
-    for i in range(1, L + 1):
-        w_norm += np.linalg.norm(weights['W' + str(i)])
-    L2 = cost + (lambtha / (2 * m) * w_norm)
-    return L2
+    l2_reg_cost = 0
+    for layer in range(1, L + 1):
+        l2_reg_cost += np.sum(np.square(weights["W" + str(layer)]))
+    l2_cost = (1/m) * (lambtha/2) * l2_reg_cost
+    return cost + l2_cost
