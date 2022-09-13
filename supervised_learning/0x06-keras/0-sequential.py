b9260e15edbabf31bd1c4fc4191f1cc2b0dfe4dc
Juansepo13 <juancho_s1314@hotmail.com>
2022-09-13 11:11:21 -0500

    add file0 task0

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
-
-# Force Seed - fix for Keras
-SEED = 0
-
-import os
-os.environ['PYTHONHASHSEED'] = str(SEED)
-import random
-random.seed(SEED)
-import numpy as np
-np.random.seed(SEED)
-import tensorflow as tf
-tf.random.set_seed(SEED)
+'''Sequential module'''
 import tensorflow.keras as K
 
-build_model = __import__('0-sequential').build_model
 
-if __name__ == '__main__':
-    network = build_model(784, [256, 256, 10], ['tanh', 'tanh', 'softmax'], 0.001, 0.95)
-    network.summary()
-    print(network.losses)
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''builds a neural network with the Keras library
+    Args:
+        nx is the number of input features to the network
+        layers is a list containing the number of nodes in each layer of the
+               network
+        activations is a list containing the activation functions used for
+                    each layer of the network
+        lambtha is the L2 regularization parameter
+        keep_prob is the probability that a node will be kept for dropout
+    Returns: the keras model
+    '''
+    w = K.initializers.VarianceScaling(mode="fan_avg")
+    regularizer = K.regularizers.l2(lambtha)
+    model = K.Sequential(name="my_sequential")
+    for i in range(len(layers)):
+        if i == 0:
+            i_shape = (nx,)
+        else:
+            i_shape = (model.layers[i - 1].output_shape[0],)
+        layer = K.layers.Dense(units=layers[i],
+                               activation=activations[i],
+                               kernel_initializer=w,
+                               kernel_regularizer=regularizer,
+                               input_shape=i_shape)
+        dropout = K.layers.Dropout(1 - keep_prob,
+                                   input_shape=i_shape)
+        model.add(layer)
+        if i < len(layers) - 1:
+            model.add(dropout)
+    return model
