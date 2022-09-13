bf13969692f39ba285ed406d3742a2a4d3d634b6
Juansepo13 <juancho_s1314@hotmail.com>
2022-09-13 11:45:31 -0500

    add file 1

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,36 +1,24 @@
+
 #!/usr/bin/env python3
-'''Sequential module'''
+"""
+    module
+"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    '''builds a neural network with the Keras library
-    Args:
-        nx is the number of input features to the network
-        layers is a list containing the number of nodes in each layer of the
-               network
-        activations is a list containing the activation functions used for
-                    each layer of the network
-        lambtha is the L2 regularization parameter
-        keep_prob is the probability that a node will be kept for dropout
-    Returns: the keras model
-    '''
-    w = K.initializers.VarianceScaling(mode="fan_avg")
-    regularizer = K.regularizers.l2(lambtha)
-    model = K.Sequential(name="my_sequential")
-    for i in range(len(layers)):
-        if i == 0:
-            i_shape = (nx,)
-        else:
-            i_shape = (model.layers[i - 1].output_shape[0],)
-        layer = K.layers.Dense(units=layers[i],
-                               activation=activations[i],
-                               kernel_initializer=w,
-                               kernel_regularizer=regularizer,
-                               input_shape=i_shape)
-        dropout = K.layers.Dropout(1 - keep_prob,
-                                   input_shape=i_shape)
-        model.add(layer)
-        if i < len(layers) - 1:
-            model.add(dropout)
-    return model
+    """ function """
+    out = K.Sequential()
+    reg = K.regularizers.L2(lambtha)
+    for idx, layer in enumerate(layers):
+        out.add(
+            K.layers.Dense(
+                layer,
+                activation=activations[idx],
+                input_shape=(nx,),
+                kernel_regularizer=reg
+            )
+        )
+        if idx < (len(layers) - 1):
+            out.add(K.layers.Dropout(1 - keep_prob))
+    return out
