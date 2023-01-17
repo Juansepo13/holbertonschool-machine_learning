3868a7372ae9859a3dc3a6bc13c0bb7a773da174
Juansepo13 <3895@holbertonschool.com>
2023-01-17 14:09:56 -0500

    add file png Task2 🤑⚽⚡

--- math/0x01-plotting/2-change_scale.py
+++ math/0x01-plotting/2-change_scale.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
+import matplotlib
+
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 
 x = np.arange(0, 28651, 5730)
@@ -14,3 +18,25 @@ plt.title('Exponential Decay of C-14')
 plt.yscale('log')
 plt.xlim(0, 28650)
 plt.show()
+plt.savefig('2-change_scale.png')
+
+# #!/usr/bin/env python3
+# import numpy as np
+# import matplotlib
+
+# matplotlib.use('Agg')
+
+# import matplotlib.pyplot as plt
+
+# mean = [69, 0]
+# cov = [[15, 8], [8, 15]]
+# np.random.seed(5)
+# x, y = np.random.multivariate_normal(mean, cov, 2000).T
+# y += 180
+
+# plt.scatter(x, y, color='m')
+# plt.xlabel('Height (in)')
+# plt.ylabel('Weight (lbs)')
+# plt.title("Men's Height vs Weight")
+# plt.show()
+# plt.savefig('1-scatter.png')
