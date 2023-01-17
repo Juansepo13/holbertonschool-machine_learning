1c7c32c5a4c146060445bc1db640b342a81316f0
Juansepo13 <3895@holbertonschool.com>
2023-01-17 17:02:44 -0500

    add Task 0 and file Png ⚡⚽👃🏻

---
+++ supervised_learning/0x07-cnn/0-main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib 
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+conv_forward = __import__('0-conv_forward').conv_forward
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    lib = np.load('../data/MNIST.npz')
+    X_train = lib['X_train']
+    m, h, w = X_train.shape
+    X_train_c = X_train.reshape((-1, h, w, 1))
+
+    W = np.random.randn(3, 3, 1, 2)
+    b = np.random.randn(1, 1, 1, 2)
+
+    def relu(Z):
+        return np.maximum(Z, 0)
+
+    plt.imshow(X_train[0])
+    plt.show()
+    plt.savefig('0-main1.png')
+    A = conv_forward(X_train_c, W, b, relu, padding='valid')
+    print(A.shape)
+    plt.imshow(A[0, :, :, 0])
+    plt.show()
+    plt.savefig('0-main2.png')
+    plt.imshow(A[0, :, :, 1])
+    plt.show()
+    plt.savefig('0-main3.png')
