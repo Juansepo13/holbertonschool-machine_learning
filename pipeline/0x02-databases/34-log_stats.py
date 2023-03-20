b056f6337155d772e520afccf20ac98939fd341c
Juansepo13 <3895@holbertonschool.com>
2023-03-20 11:15:21 -0500

    add fixing file 34

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://localhost:27017')
     logs_coll = client.logs.nginx
     doc_count = logs_coll.count_documents({})
     print("{} logs".format(doc_count))
@@ -20,4 +20,3 @@ if __name__ == "__main__":
     filter_path = {"method": "GET", "path": "/status"}
     path_count = logs_coll.count_documents(filter_path)
     print("{} status check".format(path_count))
-    
